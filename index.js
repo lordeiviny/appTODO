75794b8517519e6935a8036f9ef2a7b151c476bb
lordeiviny <deiviny_willian@hotmail.com>
2023-05-24 21:31:25 -0300

    novo

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 const { PrismaClient } = require('@prisma/client');
 const express = require('express');
+const bcrypt = require('bcrypt');
 const app = express();
 app.use(express.json());
 
 const prisma = new PrismaClient();
 
+// Rota de registro de usuários
+app.post('/users/register', async (req, res) => {
+  const { nome, email, senha } = req.body;
+
+  try {
+    // Hash da senha
+    const hashedPassword = await bcrypt.hash(senha, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        nome,
+        email,
+        senha: hashedPassword,
+      },
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao registrar usuário.' });
+  }
+});
+
+// Rota de login
+app.post('/users/login', async (req, res) => {
+  const { email, senha } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.status(404).json({ error: 'Usuário não encontrado.' });
+    }
+
+    // Comparação da senha fornecida com o hash armazenado
+    const isPasswordValid = await bcrypt.compare(senha, user.senha);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Credenciais inválidas.' });
+    }
+
+    res.json({ message: 'Login bem-sucedido.' });
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao fazer login.' });
+  }
+});
+
+// Rotas existentes para tarefas
 app.get('/tasks', async (req, res) => {
   const tasks = await prisma.task.findMany();
   res.json(tasks);
@@ -16,8 +61,8 @@ app.post('/tasks', async (req, res) => {
     data: {
       nome,
       descricao,
-      isDone: false
-    }
+      isDone: false,
+    },
   });
   res.status(201).json(newTask);
 });
@@ -29,13 +74,13 @@ app.put('/tasks/:id', async (req, res) => {
   try {
     const updatedTask = await prisma.task.update({
       where: {
-        id: taskId
+        id: taskId,
       },
       data: {
         nome,
         descricao,
-        isDone
-      }
+        isDone,
+      },
     });
     res.json(updatedTask);
   } catch (error) {
@@ -49,8 +94,8 @@ app.delete('/tasks/:id', async (req, res) => {
   try {
     await prisma.task.delete({
       where: {
-        id: taskId
-      }
+        id: taskId,
+      },
     });
     res.sendStatus(204);
   } catch (error) {
@@ -59,6 +104,4 @@ app.delete('/tasks/:id', async (req, res) => {
 });
 
 const port = 3000;
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
+app.listen
